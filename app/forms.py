b5f374eda9a196793c4dd789883ecf48c5e424df
shubhampatidar6055 <shubhampatidar6055@.com>
2024-07-23 21:42:48 +0530

    Setup project

---
+++ app/forms.py
@@ -0,0 +1,7 @@
+from .models import *
+from django import forms
+
+# class Usermodelform(forms.ModelForm):
+#     class Meta:
+#         model = Usermodel
+#         fields = ["name", "description"]
